148ea0f2d6c859fd951cd60bb0260a671628689b
박상은 <146156429+sangeun0612@users.noreply.github.com>
2025-09-05 22:13:16 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+
+    for(int i=0; i<a; i++){
+        for(int j=0; j<b; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+}

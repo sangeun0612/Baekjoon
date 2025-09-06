d0b0de5d963ba67de643244429293c6135f2fcbe
박상은 <146156429+sangeun0612@users.noreply.github.com>
2025-09-06 13:26:30 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a*3 + b*4 + c*5;
+}
